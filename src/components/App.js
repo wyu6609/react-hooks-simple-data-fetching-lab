2f504b7ff1543e5c29d9726a314e9eafe289650c
William Yu <wy427@nyu.edu>
2022-02-27 20:16:15 -0500

    assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+const uri = "https://dog.ceo/api/breeds/image/random";
+
+const App = () => {
+  const [render, setRender] = useState("Loading...");
+  useEffect(() => {
+    fetch(uri)
+      .then((resp) => resp.json())
+      .then((data) => setRender(<img src={data.message} alt="A Random Dog" />));
+  }, []);
+  return <p>{render}</p>;
+};
+
+export default App;
